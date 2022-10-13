553de69e4bff27441641030312fb1746c0fe354b
SEO JAE WON <yontanine@kakaosign.com>
2022-10-13 16:44:47 +0900

    feat: 불필요한 컴포넌트 삭제

--- packages/nextjs-boilerplate/.storybook/main.js
+++ packages/nextjs-boilerplate/.storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
   "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../components/**/*.components.mdx",
+    "../components/**/*.components.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
